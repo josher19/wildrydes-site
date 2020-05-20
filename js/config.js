0114723171d6275b3b53d704a30bea7588cc1643
josher19 <josher19@users.sf.net>
2020-05-20 23:16:31 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_ZSZXbPDSd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1qj42bpmjuel2jik03df84inta', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_85qwlxjLe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2u4qfu5tkvucmh24tam2521njd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qs0xya9u12.execute-api.ap-southeast-1.amazonaws.com/prod',
+        invokeUrl: '',
     }
 };
