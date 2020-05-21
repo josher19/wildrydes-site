58d5bd18d7f1b2d4fe96e3295d80dc363d647ba0
josher19 <josher19@users.sf.net>
2020-05-21 00:03:06 +0000

    Add invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '',
+        invokeUrl: 'https://cop3mgw2ej.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
